7f7b4090723613a193289833190a9f0d262c5409
jdiazc2000 <i201913099@cibertec.edu.pe>
2022-01-15 22:22:12 -0500

    2do commit

--- jsp.js
+++ jsp.js
@@ -11,3 +11,19 @@ navtoogle.addEventListener("click", () => {
         document.querySelector('.menuicon').style.transform = 'rotate(0deg)'
     }
 })
+
+//Portada Parallax//
+const header = document.querySelector(".title-hero");
+const headerinfo = document.querySelector(".subtitle-hero");
+const background = document.querySelector(".parallax-img:nth-child(1)");
+const text = document.querySelector(".content-body");
+
+window.addEventListener("scroll", function() {
+    const scroll = window.pageYOffset;
+    background.style.top = scroll / 1.4 + "px";
+    header.style.opacity = 1 - scroll / 300;
+    headerinfo.style.opacity = 1 - scroll / 300;
+    if (scroll > 100 && scroll < 300) {
+        text.style.bottom = scroll / 2 + "px";
+    }
+});
