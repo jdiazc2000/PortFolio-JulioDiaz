8f49b3f553058462763894929e92e6472797a01a
jdiazc2000 <Julio_Diaz_Cortegana@hotmail.com>
2022-02-16 23:24:21 -0500

    LINKS FIXED

--- Js/jsp.js
+++ Js/jsp.js
@@ -1,21 +1,104 @@
-function closeMenu() { document.querySelector(".nav-menu").className = "nav-menu", document.querySelector(".menuicon").style.transform = "rotate(0deg)", document.querySelector("body").style.overflowY = "scroll" }
+//Nav//
+const navtoogle = document.querySelector(".nav-toogle");
+const navmenu = document.querySelector(".nav-menu");
 
-async function handleSubmit(t) {
-    t.preventDefault(), button.classList.toggle("button_loading"), button.style.backgroundColor = "rgb(207, 64, 64)", button.style.color = "white", button.style.border = "solid white", button.style.boxShadow = "0px 1px 20px rgba(255, 255, 255, 0.8)", button.style.transition = ".2s ease-in-out";
-    const e = new FormData(this),
-        o = await fetch(this.action, { method: this.method, body: e, headers: { Accept: "application/json" } });
-    o.ok ? (button.classList.remove("button_loading"), this.reset(), Swal.fire("Correo enviado con éxito", "¡Gracias por ver mi portafolio, intentaré comunicarme contigo lo más pronto posible :)!", "success"), button.style.backgroundColor = "", button.style.color = "", button.style.border = "", button.style.boxShadow = "", button.style.transition = ".2s ease-in-out") : (button.classList.remove("button_loading"), Swal.fire("Error", "Ingrese datos en el formulario.", "error"), button.style.backgroundColor = "", button.style.color = "", button.style.border = "", button.style.boxShadow = "", button.style.transition = ".2s ease-in-out")
+navtoogle.addEventListener("click", () => {
+    navmenu.classList.toggle("nav-menu_visible");
+    if (navmenu.classList.contains("nav-menu_visible")) {
+        document.querySelector('.menuicon').style.transform = 'rotate(90deg)'
+        document.querySelector('body').style.overflowY = 'hidden'
+    } else {
+        document.querySelector('.menuicon').style.transform = 'rotate(0deg)'
+        document.querySelector('body').style.overflowY = 'scroll'
+    }
+})
+
+function closeMenu() {
+    document.querySelector(".nav-menu").className = "nav-menu";
+    document.querySelector('.menuicon').style.transform = 'rotate(0deg)'
+    document.querySelector('body').style.overflowY = 'scroll'
 }
-const navtoogle = document.querySelector(".nav-toogle"),
-    navmenu = document.querySelector(".nav-menu");
-navtoogle.addEventListener("click", () => { navmenu.classList.toggle("nav-menu_visible"), navmenu.classList.contains("nav-menu_visible") ? (document.querySelector(".menuicon").style.transform = "rotate(90deg)", document.querySelector("body").style.overflowY = "hidden") : (document.querySelector(".menuicon").style.transform = "rotate(0deg)", document.querySelector("body").style.overflowY = "scroll") });
-const header = document.querySelector(".texts"),
-    background = document.querySelector(".parallax-img:nth-child(1)"),
-    text = document.querySelector(".content-body");
+
+
+//Banner Parallax//
+const header = document.querySelector(".texts");
+const background = document.querySelector(".parallax-img:nth-child(1)");
+const text = document.querySelector(".content-body");
+
 window.addEventListener("scroll", function() {
-    const t = window.pageYOffset;
-    background.style.top = t / 3 + "px", header.style.opacity = 1 - t / 400
+    const scroll = window.pageYOffset;
+    background.style.top = scroll / 3.0 + "px";
+    header.style.opacity = 1 - scroll / 400;
 });
-const $form = document.querySelector("#contacto"),
-    button = document.querySelector(".button");
-$form.addEventListener("submit", handleSubmit);
+
+
+//Contact Form//
+const $form = document.querySelector("#contacto");
+const button = document.querySelector(".button");
+$form.addEventListener('submit', handleSubmit)
+
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    button.classList.toggle("button_loading");
+    button.style.backgroundColor = 'rgb(207, 64, 64)';
+    button.style.color = 'white';
+    button.style.border = 'solid white';
+    button.style.boxShadow = '0px 1px 20px rgba(255, 255, 255, 0.8)';
+    button.style.transition = '.2s ease-in-out';
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok) {
+        button.classList.remove("button_loading");
+        this.reset();
+        Swal.fire(
+            'Correo enviado con éxito',
+            '¡Gracias por ver mi portafolio, intentaré comunicarme contigo lo más pronto posible :)!',
+            'success'
+        )
+        button.style.backgroundColor = '';
+        button.style.color = '';
+        button.style.border = '';
+        button.style.boxShadow = '';
+        button.style.transition = '.2s ease-in-out';
+    } else {
+        button.classList.remove("button_loading");
+        Swal.fire(
+            'Error',
+            'Ingrese datos en el formulario.',
+            'error'
+        )
+        button.style.backgroundColor = '';
+        button.style.color = '';
+        button.style.border = '';
+        button.style.boxShadow = '';
+        button.style.transition = '.2s ease-in-out';
+    }
+}
+
+
+/* Links Functions */
+const PokedexLink = document.getElementById('Pokedex-link');
+const PokedexLink_GitHub = document.getElementById('Github-Pokedex-link');
+
+PokedexLink.addEventListener('click', function(){
+    event.preventDefault();
+    if(confirm('¿Deseas ser redireccionado al Pokédex?'))
+    {
+        window.open("https://pokedex-juliodiaz.netlify.app/");
+    }
+})
+
+PokedexLink_GitHub.addEventListener('click', function(){
+    event.preventDefault();
+    if(confirm('¿Deseas ser redireccionado al repositorio del Pokédex?'))
+    {
+        window.open("https://github.com/jdiazc2000/PokeDex");
+    }
+})
